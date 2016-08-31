131f2da694c7393ebce28ff245fe08c815e8ab68
oliweiradf <oliweira@hotmail.com>
2016-08-31 17:26:02 -0300

    Comitando todos os arquivos.

---
+++ app/src/androidTest/java/oliweira/com/br/contacerta/ApplicationTest.java
@@ -0,0 +1,13 @@
+package oliweira.com.br.contacerta;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
